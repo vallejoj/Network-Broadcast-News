9b0bda048d6faf97daed470e0789d9ee8311c818
Joshua Vallejo <joshua.a.vallejo@gmail.com>
2017-07-01 16:07:41 -1000

    I have done a lot and nothing at all.

---
+++ practice for putting up a websitre/practice.js
@@ -0,0 +1,20 @@
+const http= require('http');
+const net = require('net');
+const fs = require('fs');
+
+
+
+var server=http.createServer(function(req,res){
+  res.writeHead(200, {'Content-Type':"text/html"})
+  var myReadStream = fs.createReadStream(__dirname + '/index.html','utf8');
+
+  myReadStream.pipe(res);
+
+});
+
+server.listen({
+  host: '127.0.0.1',
+  port: 6969,
+  exclusive: true
+});
+console.log('yooooo')
