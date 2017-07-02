18e75c49c67e29b83fc6dfc2fdae3cf0e5d11792
Joshua Vallejo <joshua.a.vallejo@gmail.com>
2017-07-01 14:11:22 -1000

    Moving through client side

--- client.js
+++ client.js
@@ -1,2 +1,11 @@
 const net = require('net');
-const http= require('http');
+const http = require('http');
+const fs = require('fs');
+
+
+var myReadStream = fs.createReadStream(__dirname + '/README.txt');
+
+myReadStream.on('data', function(chunk){
+  console.log('new chun recieved');
+  console.log(chunk)
+})
