c5eb2a51c23fceeed7f36d11615b77813f71890b
Joshua Vallejo <joshua.a.vallejo@gmail.com>
2017-07-05 19:24:10 -1000

    Cleaning Spacing

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const net = require('net');
 
-
 var clients= [];
 
 net.createServer(function(socket){
@@ -11,19 +10,15 @@ socket.name = socket.remoteAddress + ":" + socket.remotePort;
 
 clients.push(socket);
 
-
-
-
  broadcast('Get your chart on! ' + "\n");
  socket.write('Enter Username:\n ');
-
 
 socket.on('data', (data) => {
     data = data.slice(0, data.length -1);
     if(socket.name === socket.remoteAddress + ":" + socket.remotePort) {
       socket.name = data;
-      broadcast(socket.name + "has joined\n", socket);
-      console.log(socket.name + "has connected")
+      broadcast(socket.name + " has joined\n", socket);
+      console.log(socket.name + " has connected")
 
     }else {
       broadcast(socket.name + ': ' + data + "\n");
@@ -33,7 +28,7 @@ socket.on('data', (data) => {
 
   socket.on('end', function () {
      clients.splice(clients.indexOf(socket), 1);
-     broadcast(socket.name + "left the chat.");
+     broadcast(socket.name + " left the chat.");
      console.log(socket.name + "left the chat.");
    });
 
